329a3966725ca423549d32e8ae763243b0377205
Roman Kolpachev <roman.kolpachev@yandex.ru>
2020-04-02 16:12:24 +0700

    Updates

---
+++ precache-manifest.a4a0942b1ccd8b11dadffa1612893cbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a34f2b1da29dd3da8f6a0fc0b313483d",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "33f9972ec04076da67dc",
+    "url": "/todo/static/css/main.fb225ce2.chunk.css"
+  },
+  {
+    "revision": "ad5f31dddd0d514acbca",
+    "url": "/todo/static/js/2.d416d9a6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todo/static/js/2.d416d9a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33f9972ec04076da67dc",
+    "url": "/todo/static/js/main.14fbc5aa.chunk.js"
+  },
+  {
+    "revision": "10fd3631c43d335a3db9",
+    "url": "/todo/static/js/runtime-main.2a8ec137.js"
+  }
+]);
